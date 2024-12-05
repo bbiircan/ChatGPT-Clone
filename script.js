faba796f5fcf868c97de8811820d632623c6d011
bbiircan <bircanbircan1963@gmail.com>
2024-12-05 17:12:44 +0300

    Working on generating API response.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 
 let userText = null;
+const API_KEY = "AIzaSyAAf5Gr-aQlNzhidMjo4_jxhh6HF8IKczk";
+
 
 const createElement = (html, className) => {
     // Create new div and apply chat, specified class and set html content of div
@@ -12,8 +14,38 @@ const createElement = (html, className) => {
     return chatDiv; // Return the created chat div
 }
 
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`;
+    const pElement = document.createElement("p");
+
+    // Define the properties and data for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            contents: [{
+                role: "user",
+                parts: [{ text: userText }]
+            }]
+        }),
+    };
+
+    // Send POST request to API, get response and set the response as paragraph element text
+    try {
+        const response = await fetch(API_URL, requestOptions);
+        const responseData = await response.json();
+        pElement.textContent = responseData.candidates[0].content.parts[0].text;
+    } catch (error) {
+        console.log(error);
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+
+}
+
 const showTypingAnimation = () => {
-    const html = `  <div class="chat-content">
+    const html = `<div class="chat-content">
                 <div class="chat-details">
                     <img src="images/chatgpt.png" alt="chatgpt-img">
                     <div class="typing-animation">
@@ -26,22 +58,25 @@ const showTypingAnimation = () => {
             </div>`;
 
     // Create an incoming chat div with typing animation and append it to chat container
-    const outgoingChatDiv = createElement(html, "incoming");
-    chatContainer.appendChild(outgoingChatDiv);
+    const incomingChatDiv = createElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    getChatResponse(incomingChatDiv);
 
 }
 
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim(); // Get chatInput value and remove extra spaces
+    if(!userText) return; // If chatInput is empty return from here
     const html = `<div class="chat-content">
                 <div class="chat-details">
                     <img src="images/user.jpeg" alt="user-img">
-                    <p>${userText}</p>
+                    <p></p>
                 </div>
             </div>`;
 
     // Create an outgoing chat div with user's message and append it to chat container
     const outgoingChatDiv = createElement(html, "outgoing");
+    outgoingChatDiv.querySelector("p").textContent = userText;
     chatContainer.appendChild(outgoingChatDiv);
     setTimeout(showTypingAnimation, 500);
 }
